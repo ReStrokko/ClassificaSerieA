a149162d0403290b29799c91a4503156f42805b5
MicM <MicM@C34>
2022-04-29 09:10:19 +0200

    Updated version.

--- ProvaClassi/Program.cs
+++ ProvaClassi/Program.cs
@@ -19,29 +19,142 @@ namespace ProvaClassi
 
     class Torneo {
 
-        Squadra[] partecipanti =new Squadra[20];
-        int[][,] Giornate= new int[34][,];
+        Squadra[] partecipanti = new Squadra[20];
+        int[][][,] Giornate = new int[34][][,];              //---> first array is for the day in wich the game was played, Second is for the match. 
+                                                             //---> The matrix has in the first row there's the team's pointer while on the second the row there's the score of each team
+        string[] lSquadre;
 
-        
         public Torneo(string[] lSquadre)
         {
+            this.lSquadre = lSquadre;
             partecipanti = new Squadra[20];
-            for (int i= 0; i < partecipanti.Length; i++)
+            for (int i = 0; i < partecipanti.Length; i++)
             {
                 partecipanti[i] = new Squadra(lSquadre[i]);
-                partecipanti[i].prova();
             }
+            InitializeDb();
             FetchMatches();
-
+            Console.WriteLine("The Match database has been succesfully loaded");
+            Console.WriteLine("Showing the Leaderboard");
+            AssingPoints();
+            Classifica();
         }
+
 
         private void FetchMatches()
         {
+            int giornata=33;
+            int partita = 0;
+            bool teamOrResult = true;          //true = teamname, false=match score
+            bool team = true;
             string path = @"C:\Users\MicM\Desktop\SerieA.txt";
             string readText = File.ReadAllText(path);  // Read the contents of the file
-            foreach (var line in readText.Split('\n'))
+            foreach (var line in readText.Split('\n'))  //read the line 
             {
-                Console.WriteLine(line);
+                //Console.WriteLine(line);
+                if (line.Contains("GIORNATA"))          //This goes trough the days meaning that there is a new set of matches
+                {
+                    giornata = Convert.ToInt32(line.Split(' ')[1])-1;
+                    partita = 0;
+                }
+                else                                        //being here means that there are matches for the day
+                {
+                    foreach (string obj in line.Split(' '))     //match
+                    {
+                        foreach (string TPorSc in obj.Split('-'))       //Team names or Scores
+                        {
+                            if (teamOrResult)
+                            {
+                                if (team)
+                                {
+                                    Giornate[giornata][partita][0, 0] = StringToTeamId(TPorSc);
+                                    StringToTeamId(TPorSc);
+                                }
+                                else
+                                {
+                                    Giornate[giornata][partita][0, 1] = StringToTeamId(TPorSc);
+                                    StringToTeamId(TPorSc);
+                                }
+                            }
+                            else
+                            {
+                                if (team)
+                                {
+                                    Giornate[giornata][partita][1, 0] = Convert.ToInt32(TPorSc);
+                                }
+                                else
+                                {
+                                    Giornate[giornata][partita][1, 1] = Convert.ToInt32(TPorSc);
+                                }
+                            }
+                            team = !team;
+                        }
+                        teamOrResult = !teamOrResult;
+                    }
+                    partita++;
+                }
+            }
+        }
+
+        private void InitializeDb()
+        {
+            for (int i = 0; i < 34; i++)
+            {
+                Giornate[i] = new int[10][,];
+                for (int j = 0; j < 10; j++)
+                {
+                    Giornate[i][j] = new int[2, 2];
+                }
+            }
+        }
+
+        private int StringToTeamId(string team)
+        {
+            int sol;
+
+            sol = Array.IndexOf(lSquadre, team);
+            return sol;
+        }
+        
+        private void AssingPoints()
+        {
+            for (int i = 0; i < 34; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    if(Giornate[i][j][1,0]> Giornate[i][j][1, 1]){partecipanti[Giornate[i][j][0, 0]].punti+=3;}
+                    if(Giornate[i][j][1,0] == Giornate[i][j][1, 1]) { partecipanti[Giornate[i][j][0, 0]].punti++; partecipanti[Giornate[i][j][0, 1]].punti++; }
+                    if(Giornate[i][j][1,1] > Giornate[i][j][1, 0]) { partecipanti[Giornate[i][j][0, 1]].punti += 3; }
+                }
+            }
+        }
+
+        private void Classifica()
+        {
+            int[,] classifica = new int[20, 2];
+            for (int i = 0; i < classifica.GetLength(0); i++)   //I copy id and points into a new matrix.
+            {
+                classifica[i, 0] = i;
+                classifica[i, 1] = partecipanti[i].punti;
+            }
+            int tempid,tempval;
+            for(int i = 0; i < classifica.GetLength(1)-1; i++)    //and sort it using a simple bubble sort not the fastest but the easiest.
+            {
+                for (int j = 0; j < classifica.GetLength(1); j++)
+                {
+                    if(classifica[i, 1] < classifica[j,1 ]){
+                        tempid = classifica[i,0] ;
+                        tempval = classifica[i,1] ;
+                        classifica[i,0]=classifica[j,0] ;
+                        classifica[i,1]=classifica[j,1] ;
+                        classifica[j,0]=tempid ;
+                        classifica[j,1]=tempval ;
+                    }
+                }
+            }
+            for(int j = 0; j < classifica.GetLength(0); j++)
+            {
+                Console.WriteLine("{0} - {1}",classifica[j,1],partecipanti[classifica[j,0]].Name);
             }
         }
     }
@@ -50,15 +163,11 @@ namespace ProvaClassi
     {
         private string name;
         public string Name { get { return name; } }
-       
+        public int punti;
         public Squadra(string nome)
         {
             name = nome;
-        }
-
-        public void prova()
-        {
-            Console.WriteLine("Squadra: {0}",name);
+            punti = 0;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-This is one of the first "Complex" example i'm doing while learning C#.
+--This is one of the first "Complex" example i'm doing while learning C#.
 
-The program fetches info about Serie A's italian football competition from a file, like it would do with a database.
-It stores it and does some manipulations.
+The program fetches info about Serie A's italian football competition from a file, like it would do with a database.It stores it and does some manipulations.
 
 The data i got was super messy. I used python to manipulate it and put it in a way i could read better with C#. Everything about that is not perfect and i also did some changes with a notepad. Still does the job for me.
 
-All of this was done in the first week of learning C#, on the second day of my stage at 2S.I. Software. Thanks for the help to them.
+All of this was done in the first week of learning C#, on the second day of my stage at 2 S.I. Software. Thanks for the help to them.
+
+
+--This next push is about the state of the code at the end of the day. The feature i wanted to implement are done. There is only a bug about points gained wich do not match to the official leaderboard. I'm debugging it as I write this the next morning.
+A feature i'd like to implement if there's any spare time is the option to have a JSON file, much like a noSQL database where i can store the data for further use.
 
 --Michele Minarelli
